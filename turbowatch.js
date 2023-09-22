1405d1c5db7e4d0b8caa36e0519a3c0a6fa8fb4d
Chris Wegrzyn <cwegrzyn@schmidtfutures.com>
2023-09-22 12:28:10 -0400

    Use turbowatch for building again

--- turbowatch.js
+++ turbowatch.js
@@ -9,11 +9,24 @@ void watch({
         ["not", ["dirname", "node_modules"]],
         ["match", "*.ts", "basename"],
       ],
-      name: "build",
+      name: "type-check",
       onChange: async ({ spawn }) => {
         await spawn`tsc --noEmit`;
-        await spawn`spack`;
       },
+      retry: { retries: 0 },
+    },
+    {
+      expression: ["dirname", __dirname],
+      // Marking this routine as non-interruptible will ensure that
+      // next dev is not restarted when file changes are detected.
+      interruptible: false,
+      name: "esbuild",
+      onChange: async ({ spawn }) => {
+        await spawn`node esbuild.config.mjs`;
+      },
+      // Enabling this option modifies what Turbowatch logs and warns
+      // you if your configuration is incompatible with persistent tasks.
+      persistent: true,
     },
   ],
 });
