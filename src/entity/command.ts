d4c43b0820bae715e1655e9cbba6f93870a1f85d
Chris Wegrzyn <cwegrzyn@schmidtfutures.com>
2024-01-22 23:00:10 -0500

    Real basic entity gen

--- src/entity/command.ts
+++ src/entity/command.ts
@@ -1,25 +1,104 @@
 import { Editor } from "obsidian";
+import { CustomSuggestModal } from "utils/suggest";
 import ForgedPlugin from "../index";
 import { OracleRoller } from "../oracles/roller";
-import { EntityModal } from "./modal";
+import {
+  EntityDescriptor,
+  EntityModal,
+  EntityResults,
+  EntitySpec,
+} from "./modal";
+
+export async function generateEntity<T extends EntitySpec>(
+  plugin: ForgedPlugin,
+  entityDesc: EntityDescriptor<T>,
+): Promise<EntityResults<T>> {
+  const { datastore } = plugin;
+  if (!datastore.ready) {
+    throw new Error("data not ready");
+  }
+  const rollContext = new OracleRoller(datastore.oracles);
+  return EntityModal.create({ app: plugin.app, entityDesc, rollContext });
+}
+
+const ENTITIES: Record<string, EntityDescriptor<EntitySpec>> = {
+  character: {
+    label: "Character",
+    nameGen: (ent) =>
+      `${ent.givenName[0]?.simpleResult}${ent.callSign.length > 0 ? ' "' + ent.callSign[0].simpleResult + '"' : ""} ${ent.familyName[0]?.simpleResult}`,
+    spec: {
+      givenName: {
+        id: "starforged/oracles/characters/name/given",
+        firstLook: true,
+      },
+      callSign: {
+        id: "starforged/oracles/characters/name/callsign",
+        firstLook: true,
+      },
+      familyName: {
+        id: "starforged/oracles/characters/name/family_name",
+        firstLook: true,
+      },
+      firstLook: {
+        id: "starforged/oracles/characters/first_look",
+        firstLook: true,
+      },
+      initialDisposition: {
+        id: "starforged/oracles/characters/initial_disposition",
+        firstLook: true,
+      },
+      role: {
+        id: "starforged/oracles/characters/role",
+        firstLook: false,
+      },
+      goal: {
+        id: "starforged/oracles/characters/goal",
+        firstLook: false,
+      },
+      revealedAspect: {
+        id: "starforged/oracles/characters/revealed_aspect",
+        firstLook: false,
+      },
+    },
+  },
+  faction: {
+    label: "Faction",
+    nameGen: (ent) => ent.name[0]?.simpleResult,
+    spec: {
+      name: {
+        id: "starforged/oracles/factions/name/template",
+        firstLook: true,
+      },
+    },
+  },
+};
 
 export async function generateEntityCommand(
   plugin: ForgedPlugin,
   editor: Editor,
 ): Promise<void> {
-  const { datastore } = plugin;
-  if (!datastore.ready) {
-    console.warn("data not ready");
-    return;
-  }
-  const rollContext = new OracleRoller(datastore.oracles);
-  const character = {
-    firstLook: {
-      id: "starforged/oracles/characters/first_look",
-    },
-    initialDisposition: {
-      id: "starforged/oracles/characters/initial_disposition",
-    },
-  };
-  new EntityModal(plugin.app, character, rollContext).open();
+  const [_key, entityDesc] = await CustomSuggestModal.select(
+    plugin.app,
+    Object.entries(ENTITIES),
+    ([_key, { label }]) => label,
+    undefined,
+    "What kind of entity?",
+  );
+  const entity = await generateEntity(plugin, entityDesc);
+  editor.replaceSelection(
+    `> [!oracle] ${entityDesc.label}: ${entityDesc.nameGen ? entityDesc.nameGen(entity) : ""}\n${Object.entries(
+      entity,
+    )
+      .flatMap(([_key, rolls]) => {
+        if (rolls.length > 0) {
+          const name = rolls[0].oracle.name;
+          return [
+            `> **${name}**: ${rolls.map((roll) => roll.simpleResult).join(", ")}\n`,
+          ];
+        } else {
+          return [];
+        }
+      })
+      .join("")}\n`,
+  );
 }
--- src/entity/modal.ts
+++ src/entity/modal.ts
@@ -1,48 +1,134 @@
-import { RollContext } from "model/oracle";
+import { Oracle, RollContext } from "model/oracle";
 import { RollWrapper } from "model/rolls";
 import { App, Modal, Setting } from "obsidian";
 
-export type EntitySpec = Record<string, { id: string }>;
+export type EntityDescriptor<T extends EntitySpec> = {
+  label: string;
+  nameGen?: (ent: EntityResults<T>) => string;
+  spec: T;
+};
+export type EntitySpec = Record<string, { id: string; firstLook: boolean }>;
+export type EntityResults<T extends EntitySpec> = {
+  [key in keyof T]: RollWrapper[];
+};
 
-export class EntityModal extends Modal {
-  public readonly results: Record<string, any> = {};
+export class EntityModal<T extends EntitySpec> extends Modal {
+  public accepted: boolean = false;
+  public readonly results: EntityResults<T>;
 
-  constructor(
+  static create<T extends EntitySpec>({
+    app,
+    entityDesc,
+    rollContext,
+  }: {
+    app: App;
+    entityDesc: EntityDescriptor<T>;
+    rollContext: RollContext;
+  }): Promise<EntityResults<T>> {
+    return new Promise((onAccept, onCancel) => {
+      new this(app, entityDesc, rollContext, onAccept, onCancel).open();
+    });
+  }
+
+  protected constructor(
     app: App,
-    public readonly entitySpec: EntitySpec,
+    public readonly entityDesc: EntityDescriptor<T>,
     public readonly rollContext: RollContext,
+    public readonly onAccept: (results: EntityResults<T>) => void,
+    public readonly onCancel: () => void,
   ) {
     super(app);
+    this.results = Object.fromEntries(
+      Object.entries(entityDesc.spec).map(([key]) => [
+        key,
+        [] as RollWrapper[],
+      ]),
+    ) as Record<keyof T, RollWrapper[]>;
+    console.log("init results: %o", this.results);
   }
 
   onOpen(): void {
     const { contentEl } = this;
 
-    const settings: Record<string, Setting> = {};
+    const settings: Record<keyof T, { setting: Setting; table: Oracle }> =
+      {} as Record<keyof T, { setting: Setting; table: Oracle }>;
 
     const render = (roll: RollWrapper): string => {
       const evaledRoll = roll.dehydrate();
       return `${evaledRoll.roll}: ${evaledRoll.results.join("; ")}`;
     };
 
-    for (const key in this.entitySpec) {
-      const { id } = this.entitySpec[key];
+    // const getTextComponent = (key: string): TextComponent | undefined => {
+    //   return settings[key]?.components.find(
+    //     (component): component is TextComponent =>
+    //       component instanceof TextComponent,
+    //   );
+    // };
+
+    const rollForKey = (key: keyof T): void => {
+      const { setting, table } = settings[key];
+      this.results[key] = [new RollWrapper(table, this.rollContext)];
+      // getTextComponent(key)?.setValue(render(this.results[key][0]));
+      setting.setName(render(this.results[key][0]));
+      setting.setDesc(table.name);
+    };
+
+    const clearKey = (key: keyof T): void => {
+      const { setting } = settings[key];
+      this.results[key] = [];
+      // getTextComponent(key)?.setValue("");
+      setting.setName("");
+      // settings[key].setName(table.name);
+    };
+
+    const { spec: entitySpec } = this.entityDesc;
+
+    contentEl.createEl("h1", { text: this.entityDesc.label });
+
+    for (const key in entitySpec) {
+      const { id } = entitySpec[key];
       const table = this.rollContext.lookup(id);
       if (!table) {
         this.close();
         throw new Error("missing table " + id);
       }
 
-      settings[key] = new Setting(contentEl)
-        .setName(table.name)
-        .setDesc("")
+      const setting = new Setting(contentEl)
+        .setName("")
+        .setDesc(table.name)
+        // .addText((text) => text.setDisabled(true).setValue(""))
         .addExtraButton((btn) =>
-          btn.setIcon("refresh-cw").onClick(() => {
-            this.results[key] = new RollWrapper(table, this.rollContext);
-            settings[key].setDesc(render(this.results[key]));
+          btn.setIcon("dices").onClick(() => {
+            rollForKey(key);
+          }),
+        )
+        .addExtraButton((btn) =>
+          btn.setIcon("delete").onClick(() => {
+            clearKey(key);
           }),
         );
+      settings[key] = { setting, table };
     }
+
+    new Setting(contentEl)
+      .addButton((btn) =>
+        btn.setButtonText("Roll First Look").onClick(() => {
+          for (const [key, spec] of Object.entries(this.entityDesc.spec)) {
+            if (spec.firstLook) {
+              rollForKey(key);
+            } else {
+              clearKey(key);
+            }
+          }
+        }),
+      )
+      .addButton((btn) =>
+        btn.setButtonText("Clear").onClick(() => {
+          for (const key of Object.keys(this.entityDesc.spec)) {
+            clearKey(key);
+          }
+        }),
+      );
 
     new Setting(contentEl)
       .addButton((btn) =>
@@ -50,20 +136,31 @@ export class EntityModal extends Modal {
           .setButtonText("Accept")
           .setCta()
           .onClick(() => {
-            // TODO: accept
-            console.log(this.results);
-            this.close();
+            this.accept();
           }),
       )
       .addButton((btn) =>
         btn.setButtonText("Cancel").onClick(() => {
-          // TODO: cancel
-          this.close();
+          this.cancel();
         }),
       );
   }
 
   onClose(): void {
     this.contentEl.empty();
+    if (!this.accepted) {
+      this.onCancel();
+    }
+  }
+
+  accept(): void {
+    this.accepted = true;
+    this.onAccept(this.results);
+    this.close();
+  }
+
+  cancel(): void {
+    this.accepted = false;
+    this.close();
   }
 }
