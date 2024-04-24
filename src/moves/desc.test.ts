609d92584718cf9d46d36e44ae94a9e0788164e1
Chris Wegrzyn <cwegrzyn@schmidtfutures.com>
2024-04-24 15:02:52 -0400

    sigma-based moved parser

--- src/moves/desc.test.ts
+++ src/moves/desc.test.ts
@@ -1,10 +1,11 @@
+import { Either, Left, Right } from "../utils/either";
 import { ActionMoveDescription, parseMove } from "./desc";
 
 describe("parseMove", () => {
-  it.each<{ line: string; result: ActionMoveDescription }>([
+  it.each<{ line: string; result: Either<string, ActionMoveDescription> }>([
     {
       line: "starforged/foo/bar: 3 +2{wits} vs 1, 3",
-      result: {
+      result: Right.create({
         name: "starforged/foo/bar",
         action: 3,
         stat: "wits",
@@ -12,11 +13,11 @@ describe("parseMove", () => {
         challenge1: 1,
         challenge2: 3,
         adds: 0,
-      },
+      }),
     },
     {
       line: "starforged/foo/bar: 3 +2{wits} +1{a long description (i did this)} vs 1, 3",
-      result: {
+      result: Right.create({
         name: "starforged/foo/bar",
         action: 3,
         stat: "wits",
@@ -24,9 +25,17 @@ describe("parseMove", () => {
         challenge1: 1,
         challenge2: 3,
         adds: 1,
-      },
+      }),
     },
-  ])("parses a standard move line $line", ({ line, result }) => {
+  ])("parses a standard move line '$line'", ({ line, result }) => {
     expect(parseMove(line)).toEqual(result);
+  });
+
+  it.each`
+    line                                  | msg
+    ${"starforged/foo/bar: 3 +2 vs 1, 3"} | ${"Expected: {"}
+  `("rejects invalid line '$line'", ({ line, msg }) => {
+    const result = parseMove(line);
+    expect(result).toMatchObject(Left.create(expect.stringMatching(msg)));
   });
 });
--- src/moves/desc.ts
+++ src/moves/desc.ts
@@ -1,4 +1,24 @@
+import {
+  Parser,
+  choice,
+  error,
+  grammar,
+  integer,
+  many,
+  map,
+  optional,
+  regexp,
+  run,
+  sequence,
+  string,
+  takeMid,
+  takeRight,
+  takeSides,
+  whitespace,
+  whole,
+} from "@nrsk/sigma";
 import { z } from "zod";
+import { Either, Left, Right } from "../utils/either";
 
 const BaseMoveDescriptionSchema = z.object({
   name: z.string(),
@@ -10,6 +30,109 @@ const BurnSchema = z.object({
   orig: z.number().int(),
   reset: z.number().int(),
 });
+
+// exploring different move formats:
+// moveId: action +val(stat) +val(bonus1) vs c1 and c2 (burn: orig>reset)
+
+// yaml:
+// moveId: move id
+// action: 3 +2:wits +1:add1 +2:add2
+// add1: reason1
+// add2: reason2
+// challenge: [1, 2]
+
+// action:
+// - roll: 3
+// - meter: 2 wits
+// - add: +1
+
+const SIGNED_INTEGER_RE = /(\+|-)(0|[1-9][0-9]*)/g;
+
+function signedInteger(): Parser<number> {
+  return error(
+    choice(
+      map(takeRight(string("+"), whole()), (num) => num),
+      map(takeRight(string("-"), whole()), (num) => -1 * num),
+    ),
+    "number prefixed with a + or a -",
+  );
+}
+
+function modifierWithDesc(): Parser<{ amount: number; desc: string }> {
+  return modifier(true) as Parser<{ amount: number; desc: string }>;
+}
+
+function modifier(
+  requireDesc: boolean = false,
+): Parser<{ amount: number; desc: string | null }> {
+  const descParser = takeMid(
+    string("{"),
+    regexp(/[^\}\{]+/g, "string without curly braces"),
+    string("}"),
+  );
+  return map(
+    sequence(signedInteger(), requireDesc ? descParser : optional(descParser)),
+    ([amount, desc]) => ({
+      amount,
+      desc,
+    }),
+  );
+}
+
+const moveGrammar = grammar({
+  moveExp(): Parser<ActionMoveDescription> {
+    return map(
+      sequence(
+        this.moveIdent,
+        string(":"),
+        takeMid(whitespace(), integer(), whitespace()),
+        modifierWithDesc(),
+        many(takeRight(whitespace(), modifier())),
+        takeMid(whitespace(), string("vs"), whitespace()),
+        takeSides(
+          whole(),
+          sequence(optional(whitespace()), string(","), optional(whitespace())),
+          whole(),
+        ),
+      ),
+      ([
+        name,
+        _colon,
+        action,
+        statMod,
+        adds,
+        _vs,
+        [challenge1, challenge2],
+      ]) => {
+        return {
+          name,
+          action,
+          stat: statMod.desc,
+          statVal: statMod.amount,
+          adds: adds.reduce((prev, { amount }) => prev + amount, 0),
+          challenge1,
+          challenge2,
+        };
+      },
+    );
+  },
+
+  moveIdent(): Parser<string> {
+    return regexp(/[a-z]\w*(\/\w+)*/gi, "move identifier");
+  },
+});
+
+export function parseMove(line: string): Either<string, ActionMoveDescription> {
+  const result = run(moveGrammar.moveExp).with(line);
+  if (result.isOk) {
+    return Right.create(result.value);
+  } else {
+    const [start, end] = result.span;
+    return Left.create(
+      `Expected: ${result.expected} at ${result.span}: ${line.slice(Math.max(0, start - 5), start)}[${line.slice(start, end + 1)}]${line.slice(end + 1)}`,
+    );
+  }
+}
 
 const ActionMoveDescriptionSchema = BaseMoveDescriptionSchema.extend({
   action: z.number().int(),
