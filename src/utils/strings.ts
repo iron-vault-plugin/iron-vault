3f366957ef2280164582c7198372acc313324b6b
Chris Wegrzyn <cwegrzyn@schmidtfutures.com>
2024-05-17 20:53:32 -0400

    Allow custom move name in "make a move" Fixes #19

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,3 +1,11 @@
 export function titleCase(str: string): string {
   return str.slice(0, 1).toUpperCase() + str.slice(1);
 }
+
+export function makeSafeForId(str: string): string {
+  return str
+    .replaceAll(/[^\w]+/g, "_")
+    .replaceAll(/_{2,}/g, "_")
+    .replaceAll(/^_|_$/g, "")
+    .toLowerCase();
+}
