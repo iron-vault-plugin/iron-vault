47a7a6604afc91a94775e5ed311462af1d9d3026
Chris Wegrzyn <cwegrzyn@schmidtfutures.com>
2024-05-20 22:30:16 -0400

    Upgrade eslint/eslint plugin Fixes #28

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      // globals: { ...globals.browser },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+    ignores: ["dist/"],
+  },
+);
