930ca1a008d3dd0ad7434b64a88549c9f9a69733
Chris Wegrzyn <chris.wegrzyn@gmail.com>
2024-05-23 08:43:08 -0400

    Stop removing spaces when creating tracks/clocks/etc (#76)
    Fixes #69

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,5 +1,9 @@
-export function titleCase(str: string): string {
+export function firstUppercase(str: string): string {
   return str.slice(0, 1).toUpperCase() + str.slice(1);
+}
+
+export function titleCase(str: string): string {
+  return str.replaceAll(/\b(?<!')(\w)/g, (_match, ch) => ch.toUpperCase());
 }
 
 export function makeSafeForId(str: string): string {
