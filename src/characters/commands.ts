f34194b72905a39bd1f8bbe43f6a6df16c3128cc
Kat Marchán <kzm@zkat.tech>
2024-06-10 00:13:15 -0700

    new, shiny asset picker
    Fixes: https://github.com/iron-vault-plugin/iron-vault/issues/178

--- src/characters/commands.ts
+++ src/characters/commands.ts
@@ -1,7 +1,7 @@
 import { type Datasworn } from "@datasworn/core";
 import { produce } from "immer";
 import IronVaultPlugin from "index";
-import { Editor, FuzzyMatch, MarkdownView } from "obsidian";
+import { App, Editor, MarkdownView, Modal } from "obsidian";
 import { createNewIronVaultEntityFile, vaultProcess } from "utils/obsidian";
 import { capitalize } from "utils/strings";
 import { CustomSuggestModal } from "utils/suggest";
@@ -18,6 +18,10 @@ import {
 } from "./assets";
 import { characterLens, createValidCharacter } from "./lens";
 import { Asset } from "@datasworn/core/dist/Datasworn";
+import { html, render } from "lit-html";
+import { map } from "lit-html/directives/map.js";
+import MiniSearch from "minisearch";
+import renderAssetCard from "./asset-card";
 
 export async function addAssetToCharacter(
   plugin: IronVaultPlugin,
@@ -44,22 +48,11 @@ export async function addAssetToCharacter(
     }
   }
 
-  const selectedAsset =
-    asset ??
-    (await CustomSuggestModal.select(
-      plugin.app,
-      availableAssets,
-      (asset) => asset.name,
-      ({ item: asset }: FuzzyMatch<Datasworn.Asset>, el: HTMLElement) => {
-        el.createEl("small", {
-          text:
-            asset.category +
-            (asset.requirement ? ` (requirement: ${asset.requirement})` : ""),
-          cls: "iron-vault-suggest-hint",
-        });
-      },
-      `Choose an asset to add to character ${lens.name.get(character)}.`,
-    ));
+  const selectedAsset = asset ?? (await AssetPickerModal.pick(plugin));
+
+  if (!selectedAsset) {
+    return;
+  }
 
   const options: [string, Datasworn.AssetOptionField][] = [];
   walkAsset(
@@ -140,4 +133,160 @@ export async function createNewCharacter(plugin: IronVaultPlugin) {
     `\n\`\`\`${pluginPrefixed("character")}\n\`\`\`\n`,
     true,
   );
+}
+
+export class AssetPickerModal extends Modal {
+  plugin: IronVaultPlugin;
+  searchIdx: MiniSearch<Asset>;
+
+  static pick(plugin: IronVaultPlugin) {
+    return new Promise<Asset | undefined>((resolve, reject) => {
+      const modal = new AssetPickerModal(
+        plugin.app,
+        plugin,
+        (asset) => {
+          modal.close();
+          resolve(asset);
+        },
+        () => {
+          modal.close();
+          reject();
+        },
+      );
+      modal.open();
+    });
+  }
+
+  constructor(
+    app: App,
+    plugin: IronVaultPlugin,
+    protected readonly onSelect: (asset: Asset) => void,
+    protected readonly onCancel: () => void,
+  ) {
+    super(app);
+    this.plugin = plugin;
+    this.searchIdx = this.makeIndex();
+  }
+
+  onOpen() {
+    this.setTitle("Add Asset to Character");
+    const { contentEl } = this;
+    contentEl.empty();
+    contentEl.toggleClass("iron-vault-modal-content", true);
+    this.render();
+  }
+
+  render(filter?: string) {
+    const { categories, total } = this.getAssetCategories(filter);
+    const tpl = html`
+      <input
+        class="search-box"
+        type="search"
+        placeholder="Filter assets..."
+        @input=${(e: Event) => {
+          const input = e.target as HTMLInputElement;
+          this.render(input.value);
+        }}
+      />
+      <ol class="iron-vault-asset-list">
+        ${map(Object.entries(categories), ([catName, category]) =>
+          this.renderCategory(catName, category, total <= 5),
+        )}
+      </ol>
+    `;
+    render(tpl, this.contentEl);
+  }
+
+  renderCategory(
+    catName: string,
+    category: { color?: string; assets: Asset[] },
+    open: boolean,
+  ) {
+    return html`
+      <li
+        class="category"
+        style=${category.color
+          ? `border-left: 6px solid ${category.color}`
+          : ""}
+      >
+        <div class="wrapper">
+          <details ?open=${open}>
+            <summary>
+              <span>${catName}</span>
+            </summary>
+          </details>
+          <ol class="content category">
+            ${map(
+              category.assets,
+              (asset) =>
+                html`<div class="wrapper">
+                  <details ?open=${open}>
+                    <summary>
+                      <span>${asset.name}</span>
+                    </summary>
+                  </details>
+                  <div class="content asset">
+                    <button type="button" @click=${() => this.onSelect(asset)}>
+                      Add this Asset
+                    </button>
+                    ${renderAssetCard(this.plugin, {
+                      id: asset._id,
+                      abilities: [true, false, false],
+                      options: {},
+                      controls: {},
+                    })}
+                  </div>
+                </div>`,
+            )}
+          </ol>
+        </div>
+      </li>
+    `;
+  }
+
+  getAssetCategories(filter?: string): {
+    categories: Record<string, { color?: string; assets: Asset[] }>;
+    total: number;
+  } {
+    const categories: Record<string, { color?: string; assets: Asset[] }> = {};
+    const results = filter
+      ? this.searchIdx.search(filter)
+      : [...this.plugin.datastore.assets.values()].map((m) => ({ id: m._id }));
+    let total = 0;
+    for (const res of results) {
+      const asset = this.plugin.datastore.assets.get(res.id)!;
+      if (!asset) {
+        console.error("couldn't find asset for", res);
+        continue;
+      }
+      const category = categories[asset.category] ?? {
+        color: asset.color,
+        assets: [],
+      };
+      category.assets.push(asset);
+      categories[asset.category] = category;
+      total += 1;
+    }
+    return { categories, total };
+  }
+
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
+
+  makeIndex() {
+    const idx = new MiniSearch({
+      fields: ["name", "category"],
+      idField: "_id",
+      searchOptions: {
+        prefix: true,
+        fuzzy: 0.3,
+        boost: { name: 2 },
+      },
+    });
+    // TODO: use the current context
+    idx.addAll([...this.plugin.datastore.assets.values()]);
+    return idx;
+  }
 }
