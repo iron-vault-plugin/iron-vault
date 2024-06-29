c817d6fefb3d3558bc68b9c9d1254805a3639d13
Chris Wegrzyn <chris.wegrzyn@gmail.com>
2024-06-29 15:30:52 -0400

    Handle missing character error on first load

--- src/sidebar/character.ts
+++ src/sidebar/character.ts
@@ -1,18 +1,34 @@
-import { render, html } from "lit-html";
+import { html, render } from "lit-html";
 
-import { activeCharacter } from "character-tracker";
+import { activeCharacter, MissingCharacterError } from "character-tracker";
 import IronVaultPlugin from "index";
-import { md } from "utils/ui/directives";
 import { Component } from "obsidian";
+import { md } from "utils/ui/directives";
 
 export default function renderIronVaultCharacter(
   containerEl: HTMLElement,
   plugin: IronVaultPlugin,
   parent: Component,
 ) {
-  const [charPath] = activeCharacter(plugin.characters);
-  render(
-    html`${md(plugin, `![[${charPath}|iv-embed]]`, ".", parent)}`,
-    containerEl,
-  );
+  try {
+    const [charPath] = activeCharacter(plugin.characters);
+    render(
+      html`${md(plugin, `![[${charPath}|iv-embed]]`, ".", parent)}`,
+      containerEl,
+    );
+  } catch (e) {
+    if (e instanceof MissingCharacterError) {
+      render(
+        html`<p>${e.message}</p>
+          <p></p>`,
+        containerEl,
+      );
+    } else {
+      render(
+        html`<p>Unexpected error when loading character:</p>
+          <pre>${e}</pre>`,
+        containerEl,
+      );
+    }
+  }
 }
