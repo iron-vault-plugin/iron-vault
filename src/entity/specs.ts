4c68ac3a85a03c11bccd822791571de6510295de
Kerk Wilson <kewilson@factsmgt.com>
2024-07-08 11:28:44 -0500

    Added Sundered Isles Settlement Entity

--- src/entity/specs.ts
+++ src/entity/specs.ts
@@ -399,4 +399,74 @@ export const ENTITIES: Record<string, EntityDescriptor<EntitySpec>> = {
       },
     },
   },
+  siSettlement: {
+    label: "Settlement",
+    collectionId: "oracle_collection:sundered_isles/settlement",
+    nameGen: (ent) =>
+      `${(ent.name_cursed && ent.name_cursed.length) ? ent.name_cursed[0].simpleResult : ent.name[0]?.simpleResult}`,
+    spec: {
+      region: {
+        id: "oracle_rollable:sundered_isles_supp/core/region",
+        firstLook: true,
+        definesAttribute: {
+          order: 1,
+          mechanism: AttributeMechanism.Snakecase,
+        },
+      },
+      location: {
+        id: "oracle_rollable:sundered_isles/settlement/location",
+        firstLook: true,
+        definesAttribute: {
+          order: 2,
+          mechanism: AttributeMechanism.Snakecase,
+        },
+      },
+      size: {
+        name: "Size",
+        id: "oracle_rollable:sundered_isles/settlement/size/{{region}}",
+        firstLook: true,
+      },
+      name: {
+        firstLook: true,
+        id: "oracle_rollable:sundered_isles/settlement/name"
+      },
+      name_cursed: {
+        id: "oracle_rollable:sundered_isles/settlement/name_cursed"
+      },
+      aesthetics: {
+        id: "oracle_rollable:sundered_isles/settlement/aesthetics",
+        firstLook: true
+      },
+      first_look: {
+        id: "oracle_rollable:sundered_isles/settlement/first_look",
+        firstLook: true
+      },
+      first_look_cursed: {
+        id: "oracle_rollable:sundered_isles/settlement/first_look_cursed"
+      },
+      controlling_faction: {
+        name: "Controlling faction",
+        id: "oracle_rollable:sundered_isles/settlement/identity/controlling_faction/{{region}}"
+      },
+      disposition: {
+        id: "oracle_rollable:sundered_isles/settlement/identity/disposition"
+      },
+      authority: {
+        id: "oracle_rollable:sundered_isles/settlement/identity/authority"
+      },
+      focus: {
+        name: "Focus",
+        id: "oracle_rollable:sundered_isles/settlement/identity/focus/{{location}}"
+      },
+      focus_cursed: {
+        id: "oracle_rollable:sundered_isles/settlement/identity/focus_cursed"
+      },
+      details: {
+        id: "oracle_rollable:sundered_isles/settlement/details"
+      },
+      details_cursed: {
+        id: "oracle_rollable:sundered_isles/settlement/details_cursed"
+      },
+    },
+  },
 };
