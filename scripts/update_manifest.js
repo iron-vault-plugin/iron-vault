9ca1e7b1cb41fcc5356b032fa4ebbe341f4c8b23
Chris Wegrzyn <chris.wegrzyn@gmail.com>
2024-08-08 22:09:48 -0400

    bumping manifest-beta version to 1.84.0-beta.1

--- scripts/update_manifest.js
+++ scripts/update_manifest.js
@@ -1,14 +1,29 @@
 #!/usr/bin/env node
 
-import { readFileSync, writeFileSync } from 'node:fs';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { readFileSync, rmSync, writeFileSync } from "node:fs";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const manifestPath = resolve(__dirname, '../manifest.json');
 
-const manifest = JSON.parse(readFileSync(manifestPath, 'utf8'));
+const RELEASE_MANIFEST = resolve(__dirname, "../manifest.json");
+const BETA_MANIFEST = resolve(__dirname, "../manifest-beta.json");
 
-manifest.version = process.argv[2];
+function writeManifest(destinationManifest, version) {
+  const manifest = JSON.parse(readFileSync(RELEASE_MANIFEST, "utf8"));
 
-writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
+  manifest.version = version;
+
+  writeFileSync(destinationManifest, JSON.stringify(manifest, null, 2));
+}
+
+const mode = process.argv[2];
+const version = process.argv[3];
+if (mode == "release") {
+  writeManifest(RELEASE_MANIFEST, version);
+  rmSync(BETA_MANIFEST, { force: true });
+} else if (mode == "beta") {
+  writeManifest(BETA_MANIFEST, version);
+} else {
+  throw new Error(`What kind of mode is ${mode}?`);
+}
