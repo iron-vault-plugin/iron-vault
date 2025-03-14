446d51feb9aee34361b5c06cd08ac8b51ac9e32f
Chris Wegrzyn <chris.wegrzyn@gmail.com>
2025-03-14 06:54:11 -0400

    Fix refreshing homebrew when file is deleted (#493)

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -9,3 +9,26 @@ export function parentFolderOf(root: string): string {
   parts.pop();
   return parts.length == 0 ? "/" : parts.join("/");
 }
+
+/**
+ * Gets the top-level parent of a file, relative to a root folder.
+ * For example, if the root folder is "world" and the file path is "world/continent/country/city.md",
+ * the top-level parent folder is "continent".
+ * @param rootFolderPath path to the root folder
+ * @param path path to the file
+ * @returns the top-level child containing the path, or null if the file is outside the root folder
+ */
+export function findTopLevelParent(
+  rootFolderPath: string,
+  path: string,
+): string | undefined {
+  if (!rootFolderPath.endsWith("/")) {
+    rootFolderPath += "/";
+  }
+  if (!path.startsWith(rootFolderPath)) {
+    return undefined;
+  }
+  const relativePath = path.substring(rootFolderPath.length);
+  const parts = relativePath.split("/");
+  return parts[0];
+}
