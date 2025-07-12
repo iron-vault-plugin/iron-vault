49a05bee5fbc61c1255b7e6fdfd9d2aada1a7a89
Chris Wegrzyn <chris.wegrzyn@gmail.com>
2025-07-12 08:18:53 -0600

    Expose entity funcs in API

--- packages/obsidian/src/api.ts
+++ packages/obsidian/src/api.ts
@@ -1,6 +1,16 @@
 import { CampaignDataContext } from "campaigns/context";
 import { CharacterActionContext } from "characters/action-context";
 import { IDataContext } from "datastore/data-context";
+import {
+  availableEntityTypes,
+  generateEntityViaModal,
+  promptForEntityType,
+} from "entity/command";
+import {
+  EntityDescriptor,
+  EntitySpec,
+  NewEntityModalResults,
+} from "entity/specs";
 import { rootLogger, setLogLevel } from "logger";
 import loglevel from "loglevel";
 import { App, getLinkpath, parseLinktext } from "obsidian";
@@ -48,6 +58,14 @@ export class IronVaultAPI {
     return this.activeCampaignContext ?? this.globalDataContext;
   }
 
+  mustActiveCampaignContext(): CampaignDataContext {
+    const context = this.activeCampaignContext;
+    if (!context) {
+      throw new Error("No active campaign context available.");
+    }
+    return context;
+  }
+
   /** Prompt the user to select a character for the given campaign context.
    * @returns promise that resolves to CharacterActionContext for the selected character, or fails
    *   if the user cancels.
@@ -68,6 +86,74 @@ export class IronVaultAPI {
     const campaign = campaignContext ?? this.activeCampaignContext;
     return (
       campaign && currentActiveCharacterForCampaign(this.plugin, campaign, true)
+    );
+  }
+
+  /** Returns the available entity types for the given campaign context. */
+  availableEntityTypes(
+    campaignContext: CampaignDataContext = this.mustActiveCampaignContext(),
+  ): [string, EntityDescriptor<EntitySpec>][] {
+    return availableEntityTypes(campaignContext);
+  }
+
+  /** Get the entity descriptor for the given entity type. */
+  getEntityDescriptor(
+    entityType: string,
+    campaignContext: CampaignDataContext = this.mustActiveCampaignContext(),
+  ): EntityDescriptor<EntitySpec> | undefined {
+    const entityTypes = this.availableEntityTypes(campaignContext);
+    const found = entityTypes.find(([type]) => type === entityType);
+    return found ? found[1] : undefined;
+  }
+
+  /** Prompts for an entity type and returns the entity descriptor for the selected entity type.
+   *
+   * @param campaignContext - the campaign context to use for available entity types. if not provided,
+   *  the currently active campaign context is used. if no active campaign, an error is thrown.
+   * @returns a promise that resolves to the selected entity descriptor.
+   */
+  promptForEntityType(
+    campaignContext: CampaignDataContext = this.mustActiveCampaignContext(),
+  ): Promise<EntityDescriptor<EntitySpec>> {
+    return promptForEntityType(this.plugin, campaignContext);
+  }
+
+  /** Generates an entity.
+   *
+   * @param entityDesc - the entity descriptor to use for generating the entity, as returned by
+   * `promptForEntityType` or `availableEntityTypes`. If not provided, {@link promptForEntityType}
+   * will be called to prompt the user for an entity type.
+   * @param campaignContext - the campaign context to use for generating the entity. if not provided,
+   *  the currently active campaign context is used. if no active campaign, an error is thrown.
+   * @returns a promise that resolves to the results of the entity generation modal.
+   *
+   * @remarks
+   * This method will prompt the user to select an entity type if `entityDesc` is not provided.
+   * It will then open a modal to generate the entity based on the selected type.
+   *
+   * @example <caption>Using a specific entity type</caption>
+   * // Must be run within a SF campaign context
+   * const ed = IronVaultAPI.getEntityDescriptor("sfPlanet");
+   * const entity = await IronVaultAPI.generateEntity(ed);
+   * for (const [k, v] of Object.entries(entity.entityProxy)) {
+   *   console.log(k, "=", v.map((roll) => roll.simpleResult).join("; "));
+   * }
+   * // region = Expanse
+   * // class = [Rocky World](datasworn:oracle_collection:starforged/planet/rocky)
+   * // name = Latona
+   * // atmosphere = None / thin
+   * // observed_from_space = [Precursor Vault](datasworn:oracle_collection:starforged/precursor_vault) (orbital)
+   * // settlements = None
+   */
+  async generateEntity(
+    entityDesc: EntityDescriptor<EntitySpec> | undefined = undefined,
+    campaignContext: CampaignDataContext = this.mustActiveCampaignContext(),
+  ): Promise<NewEntityModalResults<EntitySpec>> {
+    entityDesc ??= await this.promptForEntityType(campaignContext);
+    return await generateEntityViaModal(
+      this.plugin,
+      campaignContext,
+      entityDesc,
     );
   }
 
